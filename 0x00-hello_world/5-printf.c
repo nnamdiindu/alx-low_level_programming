610bf7a88214bc1363305ab3620cafd9c9c58927
Nnamdi Odi <luckyodi310@gmail.com>
2022-09-19 14:53:27 -0700

    Readjust the 5-printf file

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: always 0
+ */
 int main(void)
 {
-	printsf("with proper grammar, but the outcome is a piece of art");
+	printf("with proper grammar, but the outcome is a piece of art");
 	return (0);
 }
