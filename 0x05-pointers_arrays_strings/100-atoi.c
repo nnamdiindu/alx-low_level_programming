9cf96377518bb91253c8521c759426652f02031c
Nnamdi Odi <luckyodi310@gmail.com>
2022-09-19 19:47:48 -0700

    Added 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -17,11 +17,11 @@ int _atoi(char *s)
 			p *= -1;
 		if (s[i] > 47 && s[i] < 58)
 		{
-													if (h < 0)
+			if (h < 0)
 				h = (h * 10) - (s[i] - '0');
 			else
 				h = (s[i] - '0') * -1;
-
+			
 			if (s[i + 1] < 48 || s[i + 1] > 57)
 				break;
 		}
